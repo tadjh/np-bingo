b6e3f69a786f18ad21ebb830a71cfbf11c82adba
TadjhBrooks <tadjh.brooks@gmail.com>
2021-04-15 07:04:05 -0700

    Enable solo mode

--- packages/client/src/Config/features.ts
+++ packages/client/src/Config/features.ts
@@ -1,5 +1,5 @@
 const config = {
-  'single-player': false, // Allow solo mode
+  'single-player': true, // Allow solo mode
   'share-room': true,
   'public-rooms': false, // TODO Only mock-up
   'game-modes': false, // TODO no game logic built for alternate modes
@@ -7,7 +7,7 @@ const config = {
   'streamer-mode': false, // TODO not implemented
   'max-room-size': 30,
   'max-active-games': 5,
-  'ball-delay': 3500,
+  'ball-delay': 3500, // 3.5 seconds before next auto-draw in solo mode
 };
 
 export default config;
