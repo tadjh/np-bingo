e595cbe39ea8cf9d25a1ede865ebf5260ffce26a
TadjhBrooks <tadjh.brooks@gmail.com>
2021-04-15 02:47:46 -0700

    Restructure

--- packages/np-bingo-client/src/Config/axios.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-  baseURL: process.env.SERVER || 'http://localhost:8082/',
-});
-
-export default instance;
--- packages/np-bingo-server/src/Utils/index.ts
+++
@@ -1,9 +0,0 @@
-export function makeID(length: number) {
-  let i;
-  let result = '';
-  const characters = 'ABCDEFGHJKLMNPQRSTUVWXYZ123456789';
-  for (i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return result;
-}
