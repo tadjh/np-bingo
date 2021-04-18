567f9941c176e8c9479c09cc6a73973e231c8b34
TadjhBrooks <tadjh.brooks@gmail.com>
2021-04-17 18:08:34 -0700

    Add Decorator for FeaturesContext

--- packages/client/src/Components/Code/Code.stories.tsx
+++ packages/client/src/Components/Code/Code.stories.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react';
 import Code, { CodeProps } from './Code';
+import { FeautresContext } from '../../Utils/contexts';
+import features from '../../Config/features';
 
 export default {
   title: 'Components/Code',
@@ -13,5 +15,15 @@ export const Default = Template.bind({});
 Default.args = {
   room: 'A1B2',
 };
+
+export const ShareDisabled = Template.bind({});
+ShareDisabled.decorators = [
+  (Story) => (
+    <FeautresContext.Provider value={{ ...features, 'share-room': false }}>
+      <Story />
+    </FeautresContext.Provider>
+  ),
+];
+ShareDisabled.args = { ...Default.args };
 
 export const Blank = Template.bind({});
