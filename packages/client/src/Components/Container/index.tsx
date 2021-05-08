ca102600f086285945001435675a3861db711c43
TadjhBrooks <tadjh.brooks@gmail.com>
2021-05-08 04:06:15 -0700

    Implement Container

---
+++ packages/client/src/Components/Container/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+interface ContainerProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export default function Container({
+  className = '',
+  children,
+  ...props
+}: ContainerProps): JSX.Element {
+  return (
+    <div
+      className={[
+        'max-w-md mx-auto relative flex flex-col min-h-screen bg-gray-100 dark:bg-gray-900 shadow-md',
+        className,
+      ].join(' ')}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
