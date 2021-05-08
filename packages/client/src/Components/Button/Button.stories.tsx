8ff8680b47abe332ff518d7e60c5b49a1516ee7a
TadjhBrooks <tadjh.brooks@gmail.com>
2021-05-08 03:45:10 -0700

    Add button component with dark mode

---
+++ packages/client/src/Components/Button/Button.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+import Button, { ButtonProps } from './';
+
+export default {
+  title: 'Components/Button',
+  component: Button,
+} as Meta;
+
+const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+
+export const Base = Template.bind({});
+Base.args = {
+  children: 'Button',
+};
+
+export const Contained = Template.bind({});
+Contained.args = {
+  ...Base.args,
+  variant: 'contained',
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  ...Contained.args,
+  disabled: true,
+};
---
+++ packages/client/src/Components/Button/index.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import Ripple from '../Ripple';
+
+export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+  variant?: 'contained';
+  type?: 'button' | 'submit' | 'reset';
+  component?: any;
+  to?: string;
+  disabled?: boolean;
+}
+
+export default function Button({
+  variant,
+  component: Component,
+  className = '',
+  children,
+  type = 'button',
+  ...props
+}: ButtonProps): JSX.Element {
+  const baseStyle =
+    'relative px-7 py-2 rounded-full transition focus:outline-none hover:shadow-xl overflow-hidden ripple-lighter dark:ripple-darker transform hover:-translate-y-0.5 active:translate-y-0';
+  const disabledStyle =
+    'disabled:opacity-50 disabled:bg-gray-400 dark:disabled:bg-gray-700 disabled:text-gray-800 dark:disabled:text-gray-400 disabled:shadow-none disabled:ripple-none disabled:cursor-default disabled:translate-y-0';
+
+  function variantStyle(
+    variant: ButtonProps['variant']
+  ): ButtonProps['className'] {
+    switch (variant) {
+      case 'contained':
+        return 'bg-blue-600 hover:bg-blue-700 dark:bg-blue-300 dark:hover:bg-blue-400 text-white dark:text-black';
+      default:
+        return 'text-blue-600 hover:text-blue-700 dark:text-blue-300 dark:hover:text-blue-400 hover:bg-gray-200';
+    }
+  }
+
+  if (Component)
+    return (
+      <Component
+        className={[
+          variantStyle(variant),
+          baseStyle,
+          disabledStyle,
+          className,
+        ].join(' ')}
+        {...props}
+      >
+        <Ripple />
+        {children}
+      </Component>
+    );
+  return (
+    <button
+      className={[
+        variantStyle(variant),
+        baseStyle,
+        disabledStyle,
+        className,
+      ].join(' ')}
+      type={type}
+      {...props}
+    >
+      <Ripple />
+      {children}
+    </button>
+  );
+}
