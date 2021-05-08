98f9a06cbd57712ef9357acbf511b235786057c6
TadjhBrooks <tadjh.brooks@gmail.com>
2021-05-08 04:35:04 -0700

    Fix Tooltip white space

--- packages/client/src/Components/Tooltip/index.tsx
+++ packages/client/src/Components/Tooltip/index.tsx
@@ -13,7 +13,7 @@ export default function Tooltip({
     <span
       className={`tooltip-t absolute -top-2 -left-1.5 p-2 transform -translate-y-full rounded-sm shadow-md bg-gray-100 dark:bg-gray-700 text-black dark:text-white text-opacity-70 border-gray-300 dark:border-gray-600 border text-xs font-mono font-bold leading-normal transition-opacity delay-300 whitespace-nowrap ${
         !isHovered ? 'opacity-0' : ''
-      }${className}`}
+      } ${className}`}
     >
       <span className="absolute box-content border-br bottom-2 left-2.5 h-4 w-4 transform translate-y-full rotate-45 rounded-br-sm bg-gray-100 dark:bg-gray-700 border-gray-300 dark:border-gray-600"></span>
       {children}
