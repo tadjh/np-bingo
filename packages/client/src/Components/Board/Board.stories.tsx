05122db8cacda06572dfb964c7084213629726e2
TadjhBrooks <tadjh.brooks@gmail.com>
2021-05-08 03:24:25 -0700

    Implement dark mode

--- packages/client/src/Components/Board/Board.stories.tsx
+++ packages/client/src/Components/Board/Board.stories.tsx
@@ -9,8 +9,8 @@ export default {
 
 const Template: Story<BoardProps> = (args) => <Board {...args} />;
 
-export const Default = Template.bind({});
-Default.args = {
+export const Base = Template.bind({});
+Base.args = {
   card: [
     2,
     27,
@@ -41,32 +41,32 @@ Default.args = {
   serial: 'EzDsBZwVig2BmAjCeqpiogDI0daaIAcSmsiAnMAsFFrMEA==',
 };
 
-export const Checked = Template.bind({});
-Checked.args = {
-  ...Default.args,
-  crossmarks: {
-    cell11: true,
-    cell12: true,
-    cell13: true,
-    cell14: true,
-    cell15: true,
-    cell2: true,
-    cell7: true,
-    cell17: true,
-    cell22: true,
-    cell9: true,
-    cell8: true,
-    cell19: false,
-    cell24: true,
-    cell18: true,
-    cell4: true,
-    cell16: true,
-  },
-};
+// export const Checked = Template.bind({});
+// Checked.args = {
+//   ...Default.args,
+//   crossmarks: {
+//     cell11: true,
+//     cell12: true,
+//     cell13: true,
+//     cell14: true,
+//     cell15: true,
+//     cell2: true,
+//     cell7: true,
+//     cell17: true,
+//     cell22: true,
+//     cell9: true,
+//     cell8: true,
+//     cell19: false,
+//     cell24: true,
+//     cell18: true,
+//     cell4: true,
+//     cell16: true,
+//   },
+// };
 
 export const Winner = Template.bind({});
 Winner.args = {
-  ...Default.args,
+  ...Base.args,
   crossmarks: {
     cell11: true,
     cell12: true,
--- packages/client/src/Components/Board/index.tsx
+++ packages/client/src/Components/Board/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Card } from '@np-bingo/types';
 import Cell from '../Cell';
 import Logo from '../Logo';
-import './style.css';
 import { initialState } from '../../Reducers/play.reducer';
 
 export interface BoardProps {
@@ -21,17 +20,29 @@ export default function Board({
   ...props
 }: BoardProps) {
   return (
-    <div className="grid-container">
-      <div className={['grid', 'shadow', winner && 'winner'].join(' ')}>
-        <div className="grid-header">
-          <Logo winner={winner} />
-        </div>
+    <div
+      className={[
+        'space-y-2 border-8 rounded-xl shadow-md overflow-hidden',
+        winner
+          ? 'bg-green-600 border-green-600'
+          : ' bg-gray-900 dark:bg-gray-800 border-gray-900 dark:border-gray-800',
+      ].join(' ')}
+    >
+      <div
+        className={[
+          'py-2 px-1 bg-gray-100',
+          winner ? 'dark:bg-gray-700' : 'dark:bg-gray-800',
+        ].join(' ')}
+      >
+        <Logo winner={winner} />
+      </div>
+      <div className="grid grid-cols-5">
         {card.map((value, index) => {
           let id = `cell${index + 1}`;
           return (
             <Cell
               id={id}
-              className={[winner && 'winner'].join('')}
+              winner={winner}
               key={id}
               index={index + 1}
               checked={winner ? crossmarks[id] || false : undefined}
@@ -42,7 +53,14 @@ export default function Board({
             </Cell>
           );
         })}
-        <div className="serial">{serial}</div>
+      </div>
+      <div
+        className={[
+          'h-3 text-white dark:text-opacity-90 text-xxs text-center font-bold align-middle',
+          winner ? 'bg-green-600' : 'bg-gray-900 dark:bg-gray-800',
+        ].join(' ')}
+      >
+        {serial}
       </div>
     </div>
   );
