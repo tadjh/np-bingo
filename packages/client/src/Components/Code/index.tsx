edcca4015750b763e15687946f72d6c96724b65a
TadjhBrooks <tadjh.brooks@gmail.com>
2021-05-10 22:14:46 -0700

    Fix text opacity

--- packages/client/src/Components/Code/index.tsx
+++ packages/client/src/Components/Code/index.tsx
@@ -20,7 +20,7 @@ export default function Code({ room = '', children }: CodeProps): JSX.Element {
       <div className="text-xs text-center dark:text-white dark:text-opacity-60">
         Room Code:
       </div>
-      <div className="flex font-mono text-3xl font-bold space-x-2 text-gray-900 dark:text-white dark:text-opacity-90">
+      <div className="flex font-mono text-3xl font-bold space-x-2 text-gray-900 dark:text-white dark:text-opacity-60 dark:hover:text-opacity-90">
         {generate(
           room !== '' ? Object.assign([], room) : [' ', ' ', ' ', ' '],
           <div className="flex justify-center items-center bg-gradient-to-b from-gray-200 dark:from-gray-500 to-gray-300 dark:to-gray-700 rounded-md w-9 h-12 shadow-inner" />
