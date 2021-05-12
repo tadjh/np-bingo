6dcd55793e7b9b4a646828096ea21f472dcaa336
TadjhBrooks <tadjh.brooks@gmail.com>
2021-05-12 02:26:00 -0700

    Commit background

---
+++ packages/client/src/Components/Background/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './style.css';
+
+export default function Background() {
+  return (
+    <div className="background bg-bingo-pattern-light dark:bg-bingo-pattern-light absolute left-0 top-0 h-full w-full bg-center animate-slide opacity-50 dark:opacity-5"></div>
+  );
+}
