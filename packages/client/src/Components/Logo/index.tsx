1af688430094bcffbab15cc6e15cc987efaacc16
TadjhBrooks <tadjh.brooks@gmail.com>
2021-05-12 02:21:49 -0700

    Convert styles to switch function

--- packages/client/src/Components/Logo/index.tsx
+++ packages/client/src/Components/Logo/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { letters } from '../../Constants';
-import './style.css';
 
 export interface LogoProps {
   home?: boolean;
@@ -8,6 +7,29 @@ export interface LogoProps {
 }
 
 export default function Logo({ home = false, winner }: LogoProps): JSX.Element {
+  /**
+   * Returns ball color or win (-1) colors
+   * @param index
+   * @returns string
+   */
+  function logoStyle(index: number): string {
+    switch (index) {
+      case -1:
+        return 'bg-green-700 dark:bg-green-600 dark:text-white';
+      case 0:
+        return 'bg-blue-bingo dark:bg-blue-300 dark:text-black';
+      case 1:
+        return 'bg-red-bingo dark:bg-red-300 dark:text-black';
+      case 2:
+        return 'bg-gray-400 dark:bg-gray-300 dark:text-black';
+      case 3:
+        return 'bg-green-bingo dark:bg-green-300 dark:text-black';
+      case 4:
+        return 'bg-orange-bingo dark:bg-orange-300 dark:text-black';
+      default:
+        throw new Error('Error in Logo style');
+    }
+  }
   return (
     <div
       className={[
@@ -19,12 +41,9 @@ export default function Logo({ home = false, winner }: LogoProps): JSX.Element {
         return (
           <div
             key={index}
-            className={[
-              'flex justify-center items-center rounded-full select-none text-white font-sans font-bold uppercase text-center',
-              `logo-item logo-item-${index + 1}`,
-              winner ? 'winner dark:text-white' : 'dark:text-black',
-              home ? 'h-20 w-20 shadow-md text-4xl' : 'h-14 w-14 text-3xl',
-            ].join(' ')}
+            className={`flex justify-center items-center rounded-full select-none text-white font-sans font-bold uppercase text-center ${
+              !winner ? logoStyle(index) : logoStyle(-1)
+            } ${home ? 'h-20 w-20 shadow-md text-4xl' : 'h-14 w-14 text-3xl'}`}
           >
             {item}
           </div>
