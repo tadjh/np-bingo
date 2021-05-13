522f0d6794107c4120772fe8dea755706ea9a254
TadjhBrooks <tadjh.brooks@gmail.com>
2021-05-13 15:10:53 -0700

    Remove animation

--- packages/client/src/Components/CircularProgress/CircularProgress.tsx
+++ packages/client/src/Components/CircularProgress/CircularProgress.tsx
@@ -17,9 +17,10 @@ export default function CircularProgress(
         fill="none"
         strokeWidth="3.6"
         strokeDasharray="126.92"
-        className="stroke-current transition-all duration-75 ease-linear"
+        className="stroke-current"
         {...props}
       ></circle>
     </svg>
   );
 }
+// transition-all duration-75 ease-linear
