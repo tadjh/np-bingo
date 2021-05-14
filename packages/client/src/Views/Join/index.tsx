fee45c3b0b45bd8606aab8eced3a1e37fbe49da0
TadjhBrooks <tadjh.brooks@gmail.com>
2021-05-13 19:52:40 -0700

    Improve Join

--- packages/client/src/Views/Join/index.tsx
+++ packages/client/src/Views/Join/index.tsx
@@ -1,15 +1,16 @@
-import React, { useCallback, useContext, useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import { Gamestate, Room } from '@np-bingo/types';
+import { Gamestate } from '@np-bingo/types';
 import { useDialog } from '../../Utils/custom-hooks';
 import RoomList from '../../Components/RoomList';
 import CodeModal from '../../Components/CodeModal';
 import Footer from '../../Components/Footer';
-import { FeautresContext } from '../../Utils/contexts';
+import { FeautresContext, GameContext } from '../../Utils/contexts';
 import Main from '../../Components/Main';
 import Header from '../../Components/Header';
 import Button from '../../Components/Button';
 import Link from '../../Components/Link';
+import socket from '../../Config/socket.io';
 
 export interface JoinProps {
   queryRoom: string | null;
@@ -25,23 +26,27 @@ export default function Join({
   solo,
 }: JoinProps) {
   const { publicRooms, allowSolo } = useContext(FeautresContext);
+  const { room, user, host } = useContext(GameContext);
   let history = useHistory();
   const [open, handleOpen, handleClose] = useDialog(false);
 
-  const join = useCallback(
-    (room: Room) => {
-      joinRoom && joinRoom(room);
-      history.push(`/play?r=${room}`);
-    },
-    [history, joinRoom]
-  );
-
+  /**
+   * Handles share link
+   */
   useEffect(() => {
     if (queryRoom === null) return;
-
     // TODO Check if room is active
-    join(queryRoom);
-  }, [queryRoom, join]);
+    joinRoom && joinRoom(room);
+  }, [queryRoom, joinRoom, room]);
+
+  /**
+   * Routes when room is updated
+   */
+  useEffect(() => {
+    if (room === '') return;
+    socket.emit('join-room', room, host.socket, user);
+    history.push(`/play?r=${room}`);
+  }, [room, host.socket, user, history]);
 
   return (
     <React.Fragment>
@@ -51,7 +56,9 @@ export default function Join({
         </h1>
       </Header>
       <Main className="flex-1 justify-around gap-y-3">
-        {publicRooms && <RoomList rooms={rooms} onClick={joinRoom} />}
+        {publicRooms && (
+          <RoomList rooms={rooms} onClick={() => joinRoom && joinRoom(room)} />
+        )}
         <div className="flex flex-col items-center gap-y-3">
           <Button
             variant="contained"
@@ -60,7 +67,11 @@ export default function Join({
           >
             Private Room
           </Button>
-          <CodeModal open={open} onClose={handleClose} onSumbit={join} />
+          <CodeModal
+            open={open}
+            onClose={handleClose}
+            onSumbit={() => joinRoom && joinRoom(room)}
+          />
           {allowSolo && (
             <Button
               component={Link}
