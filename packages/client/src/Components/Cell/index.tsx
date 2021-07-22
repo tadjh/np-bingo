7cd00ac5cebe99993e2df28a60331f6d881f06fd
TadjhBrooks <tadjh.brooks@gmail.com>
2021-07-21 21:01:24 -0700

    Add scribble sfx

--- packages/client/src/Components/Cell/index.tsx
+++ packages/client/src/Components/Cell/index.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import Ripple from '../Ripple';
 import './style.css';
+import useSound from 'use-sound';
+import scribbleSfx from '../../Assets/Sounds/Scribble_Erase.mp3';
+import { SpriteMap } from 'use-sound/dist/types';
 
 export interface CellProps extends React.HTMLAttributes<HTMLDivElement> {
   winner: boolean;
@@ -36,13 +39,26 @@ export default function Cell({
     setChecked(false);
   }, [override]);
 
+  const scribbleSfxSpriteMap = {
+    scribble: [500, 500],
+    erase: [1500, 500],
+  } as SpriteMap;
+  const [playSfx] = useSound(scribbleSfx, {
+    volume: 0.25,
+    sprite: { ...scribbleSfxSpriteMap },
+  });
+
   return (
     <div
       className={[
-        'flex justify-center items-center relative w-16 h-14 ripple-lighter dark:ripple-darker select-none bg-gray-100 hover:bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600 text-black dark:text-white text-opacity-90 dark:text-opacity-90 border-2 border-gray-900 dark:border-white dark:border-opacity-5 font-mono font-bold text-2xl uppercase transition-colors cursor-pointer',
+        'flex justify-center items-center relative w-16 h-14 ripple-lighter dark:ripple-darker select-none bg-gray-100 hover:bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600 text-black dark:text-white text-opacity-90 dark:text-opacity-90 border-2 border-gray-900 dark:border-white dark:border-opacity-5 font-mono font-bold text-2xl uppercase transition-colors cursor-pointer overflow-hidden',
         `cell-${index}`,
+        `${index === 13 ? 'text-lg' : ''}`,
       ].join(' ')}
       onClick={eventHandler}
+      onMouseDown={() => {
+        !checked ? playSfx({ id: 'scribble' }) : playSfx({ id: 'erase' });
+      }}
       {...props}
     >
       <Ripple />
