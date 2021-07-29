15f5653fadaa8e0ff705ba3909171a448ceed76c
TadjhBrooks <tadjh.brooks@gmail.com>
2021-07-29 04:49:54 -0700

    Add Icon menu to Widgets

--- packages/client/src/Components/Widgets/index.tsx
+++ packages/client/src/Components/Widgets/index.tsx
@@ -1,9 +1,9 @@
 import React, { useContext } from 'react';
 import Share from '../Share';
 import { Gamemode, Room } from '@np-bingo/types';
-import ThemeToggle from '../ThemeToggle';
 import Code from '../Code';
 import { FeautresContext } from '../../Utils/contexts';
+import IconMenu from '../IconMenu';
 
 export interface WidgetProps {
   variant?: Gamemode;
@@ -15,16 +15,16 @@ export default function Widgets({
   room = '',
 }: WidgetProps): JSX.Element {
   const { share } = useContext(FeautresContext);
-  if (variant === 'solo') return <ThemeToggle />;
+  if (variant === 'solo') return <IconMenu direction="up" />;
   return (
     <div className="flex gap-5">
-      <div className="flex items-end">
-        <ThemeToggle />
-      </div>
+      <div className="flex items-end">{share && <Share room={room} />}</div>
       <div>
         <Code room={room} />
       </div>
-      <div className="flex items-end">{share && <Share room={room} />}</div>
+      <div className="flex items-end">
+        <IconMenu direction="up" />
+      </div>
     </div>
   );
 }
