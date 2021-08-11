77c57ec2be2d239a0cd8431a44b00fe80d8f6709
TadjhBrooks <tadjh.brooks@gmail.com>
2021-08-11 16:45:23 -0700

    Limit reconnection attempts

--- packages/client/src/Config/socket.io.ts
+++ packages/client/src/Config/socket.io.ts
@@ -2,6 +2,7 @@ import { io } from 'socket.io-client';
 
 const socket = io(process.env.REACT_APP_SERVER || 'http://localhost:8082/', {
   withCredentials: true,
+  reconnectionAttempts: 10,
 });
 
 // TODO Limit reconnectionAttempts
