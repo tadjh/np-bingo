1c451204d0355bcb66fbfb105097b90df6e4c6ef
TadjhBrooks <tadjh.brooks@gmail.com>
2021-08-18 02:31:06 -0700

    Fix Icon Button

--- packages/client/src/Components/Inputs/IconButton/components/IconButton/index.tsx
+++ packages/client/src/Components/Inputs/IconButton/components/IconButton/index.tsx
@@ -16,7 +16,7 @@ export interface IconButtonProps
 export default function IconButton({
   className = '',
   children,
-  disabled,
+  disabled = false,
   description = '',
   direction,
   isHovered,
@@ -35,11 +35,13 @@ export default function IconButton({
         </Tooltip>
       )}
       <IconButtonBase
-        className={`relative tooltip bg-transparent rounded-full p-2 transition ripple-lighter dark:ripple-darker focus:outline-none overflow-hidden ${
+        className={[
+          'relative tooltip bg-transparent rounded-full p-2 transition ripple-lighter dark:ripple-darker focus:outline-none overflow-hidden',
           !disabled
             ? 'hover:bg-gray-900 dark:hover:bg-white hover:bg-opacity-10 dark:hover:bg-opacity-10 hover:shadow-xl dark:hover:shadow-xl'
-            : 'cursor-default'
-        } ${className}`}
+            : 'cursor-default',
+          className,
+        ].join(' ')}
         disabled={disabled}
         {...props}
       >
