96a769c4a4309a3034eefb575fe3c492d570baeb
TadjhBrooks <tadjh.brooks@gmail.com>
2021-08-18 02:22:30 -0700

    Update TODO

--- packages/client/src/Components/Surfaces/Background/index.tsx
+++ packages/client/src/Components/Surfaces/Background/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+// TODO Add story
 export default function Background() {
   return (
     <div className="background bg-bingo-light dark:bg-bingo-dark bg-cover bg-no-repeat bg-bottom absolute left-0 top-0 h-full w-full"></div>
