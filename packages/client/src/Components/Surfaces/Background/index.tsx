74c822afbeedb8aa10f224cd8f25679bd14ed197
TadjhBrooks <tadjh.brooks@gmail.com>
2021-08-18 21:40:06 -0700

    Add BackgroundProps

--- packages/client/src/Components/Surfaces/Background/index.tsx
+++ packages/client/src/Components/Surfaces/Background/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 // TODO Add story
+
+export interface BackgroundProps {}
 export default function Background() {
   return (
     <div className="background bg-bingo-light dark:bg-bingo-dark bg-cover bg-no-repeat bg-bottom absolute left-0 top-0 h-full w-full"></div>
