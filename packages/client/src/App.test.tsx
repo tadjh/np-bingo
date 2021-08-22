7203cbb405ce09e1ee5d6ff6dfb99804c0f96aac
TadjhBrooks <tadjh.brooks@gmail.com>
2021-08-21 19:19:19 -0700

    Add skip test

---
+++ packages/client/src/App.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+// TODO write test?
+test.skip('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
