afdd278ce0b3ef5140e27a2abad02f4e19deffa0
TadjhBrooks <tadjh.brooks@gmail.com>
2021-08-22 13:08:00 -0700

    Fix env variables

--- packages/server/src/config/index.ts
+++ packages/server/src/config/index.ts
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 export const DB_USER = process.env.DB_USER || 'admin';
 export const DB_PASS = process.env.DB_PASS || 'password';
 export const DB_URL = process.env.DB_URL || 'example.com';
