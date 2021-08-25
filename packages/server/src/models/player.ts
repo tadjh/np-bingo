27d535ca72e2e3a5b813c219a717d650ef13f365
TadjhBrooks <tadjh.brooks@gmail.com>
2021-08-25 12:30:05 -0700

    Improving typing

--- packages/server/src/models/player.ts
+++ packages/server/src/models/player.ts
@@ -1,8 +1,13 @@
 import { Schema, model, Document } from 'mongoose';
 import { Player } from '@np-bingo/types';
 
+type PlayerOmit = Omit<Player, 'socketId'>;
+interface ModifiedPlayer extends PlayerOmit {
+  socketId: string;
+}
+
 // TODO Does this work?
-export type IPlayer = Player & Document;
+export type IPlayer = ModifiedPlayer & Document;
 
 export const PlayerSchema = new Schema({
   // uid: {
