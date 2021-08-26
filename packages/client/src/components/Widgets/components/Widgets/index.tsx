cc94d6f1d437198e5f70a9524e9d8dd1a88f5da0
TadjhBrooks <tadjh.brooks@gmail.com>
2021-08-26 12:11:07 -0700

    Remove Widgets variants

--- packages/client/src/components/Widgets/components/Widgets/index.tsx
+++ packages/client/src/components/Widgets/components/Widgets/index.tsx
@@ -1,23 +1,18 @@
 import { useContext } from 'react';
 import Share from '../Share';
-import { Gamemode, Room } from '@np-bingo/types';
+import { Room } from '@np-bingo/types';
 import Code from '../Code';
 import { FeaturesContext } from '../../../../context';
 import IconMenu from '../../../Inputs/IconMenu';
 
 export interface WidgetProps {
-  variant?: Gamemode;
   room?: Room;
 }
 
-export default function Widgets({
-  variant = 'default',
-  room = '',
-}: WidgetProps): JSX.Element {
+export default function Widgets({ room = '' }: WidgetProps): JSX.Element {
   const { allowShare } = useContext(FeaturesContext);
-  if (variant === 'solo') return <IconMenu direction="up" />;
   return (
-    <div className="flex gap-4">
+    <div className="flex gap-2">
       <div className="flex justify-center items-center w-14">
         {allowShare && <Share room={room} />}
       </div>
