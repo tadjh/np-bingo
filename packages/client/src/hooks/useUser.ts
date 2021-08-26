7dfa0b8001fa2e50a4c14abc61615fa198f9a0d6
TadjhBrooks <tadjh.brooks@gmail.com>
2021-08-26 12:25:21 -0700

    Generate four random digital at end of Player name

--- packages/client/src/hooks/useUser.ts
+++ packages/client/src/hooks/useUser.ts
@@ -1,8 +1,9 @@
-import { Dispatch, useReducer } from 'react';
+import { Dispatch, useMemo, useReducer } from 'react';
 import { Player } from '@np-bingo/types';
 import { NODE_ENV } from '../config';
 import logger from 'use-reducer-logger';
 import { UserActions, UserState, userReducer } from '../reducers/user.reducer';
+import { fourRandomDigits } from '../utils';
 
 // TODO Add unique string to end of player name #4140
 export const initalPlayer: Player = {
@@ -16,17 +17,24 @@ export const initalPlayer: Player = {
 
 export function useUser(
   initialUser: Player = initalPlayer
-): [Player, Dispatch<UserActions>] {
+): [UserState, Dispatch<UserActions>] {
+  const memoNumber = useMemo(() => fourRandomDigits(), []);
+
+  const formattedName = initialUser.name + '#' + memoNumber;
+
   const userInititalState: UserState = {
-    user: initialUser,
-    isLoading: false,
-    isError: false,
+    user: {
+      ...initialUser,
+      name: formattedName,
+    },
+    isSocketLoading: false,
+    isSocketError: false,
   };
-  const [{ user }, userDispatch] = useReducer<
+  const [userState, userDispatch] = useReducer<
     (state: UserState, action: UserActions) => UserState
   >(
     NODE_ENV === 'development' ? logger(userReducer) : userReducer,
     userInititalState
   );
-  return [user, userDispatch];
+  return [userState, userDispatch];
 }
