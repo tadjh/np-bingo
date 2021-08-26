839764e0c388def9f0a28f8462a1378de411ba48
TadjhBrooks <tadjh.brooks@gmail.com>
2021-08-26 12:06:48 -0700

    Update TODO

--- packages/client/src/components/Display/Typography/index.tsx
+++ packages/client/src/components/Display/Typography/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
-export interface StatusProps extends React.HtmlHTMLAttributes<HTMLDivElement> {}
+export interface TypographyProps
+  extends React.HtmlHTMLAttributes<HTMLDivElement> {}
 
 export default function Typography({
+  className,
   children,
   ...props
-}: StatusProps): JSX.Element {
+}: TypographyProps): JSX.Element {
   return (
-    <div className="text-black dark:text-white text-opacity-90 dark:text-opacity-90">
+    <div
+      className={
+        className ||
+        'text-black dark:text-white text-opacity-90 dark:text-opacity-90'
+      }
+    >
       {children}
     </div>
   );
