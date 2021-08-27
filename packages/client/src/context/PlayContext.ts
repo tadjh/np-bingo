53c60e4888fc4309e7e7007ba71ea3f81664c7cf
TadjhBrooks <tadjh.brooks@gmail.com>
2021-08-27 10:54:05 -0700

    Deprecate

--- packages/client/src/context/PlayContext.ts
+++ packages/client/src/context/PlayContext.ts
@@ -1,25 +1,27 @@
-import React, { Dispatch } from 'react';
-import { Card, Kicked, Serial } from '@np-bingo/types';
-import { PlayActions } from '../reducers/play.reducer';
+// import React, { Dispatch } from 'react';
+// import { Card, Kicked, Serial } from '@np-bingo/types';
+// import { PlayActions } from '../reducers/play.reducer';
 
-export interface PlayContextProps {
-  card: Card;
-  serial: Serial;
-  crossmarks: { [key: string]: boolean };
-  kicked: Kicked;
-  isWinner: boolean;
-  isNewGame: boolean;
-  playDispatch: Dispatch<PlayActions>;
-}
+// export interface PlayContextProps {
+//   card: Card;
+//   serial: Serial;
+//   crossmarks: { [key: string]: boolean };
+//   kicked: Kicked;
+//   isWinner: boolean;
+//   isNewGame: boolean;
+//   playDispatch: Dispatch<PlayActions>;
+// }
 
-export const initialPlayContext: PlayContextProps = {
-  card: [],
-  serial: '',
-  crossmarks: {},
-  kicked: { status: false, reason: 'none' },
-  isWinner: false,
-  isNewGame: true,
-  playDispatch: () => {},
-};
+// export const initialPlayContext: PlayContextProps = {
+//   card: [],
+//   serial: '',
+//   crossmarks: {},
+//   kicked: { status: false, reason: 'none' },
+//   isWinner: false,
+//   isNewGame: true,
+//   playDispatch: () => {},
+// };
 
-export const PlayContext = React.createContext(initialPlayContext);
+// export const PlayContext = React.createContext(initialPlayContext);
+// TODO Deprecate
+export {};
