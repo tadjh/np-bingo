1abaf7be1dd97fed4a48c477c008803487143248
TadjhBrooks <tadjh.brooks@gmail.com>
2021-08-27 10:56:02 -0700

    Deprecate usePlayState

--- packages/client/src/hooks/usePlayState.ts
+++ packages/client/src/hooks/usePlayState.ts
@@ -1,71 +1,63 @@
-import {
-  initialPlayState,
-  PlayActions,
-  PlayState,
-  reducer,
-} from '../reducers/play.reducer';
-import { useReducer } from 'react';
-import { NODE_ENV } from '../config';
-import logger from 'use-reducer-logger';
+// import {
+//   initialPlayState,
+//   PlayActions,
+//   PlayState,
+//   playReducer,
+// } from '../reducers/play.reducer';
+// import { useReducer } from 'react';
+// import { NODE_ENV } from '../config';
+// import logger from 'use-reducer-logger';
 
-export function usePlayState() {
-  const [playState, playDispatch] = useReducer<
-    (state: PlayState, action: PlayActions) => PlayState
-  >(NODE_ENV === 'development' ? logger(reducer) : reducer, initialPlayState);
+// TODO Deprecate
+export {};
 
-  // const initPlay = () => {
-  //   playDispatch({ type: INIT_GAME });
-  // };
+// export function usePlayState() {
+//   const [playState, playDispatch] = useReducer<
+//     (state: PlayState, action: PlayActions) => PlayState
+//   >(
+//     NODE_ENV === 'development' ? logger(playReducer) : playReducer,
+//     initialPlayState
+//   );
 
-  // /**
-  //  * Creates a new card and stores it in state
-  //  */
-  // const setCard = () => {
-  //   const [card, serial] = newCard(BINGO);
-  //   playDispatch({ type: NEW_CARD, payload: { card: card, serial: serial } });
-  // };
+// const initPlay = () => {
+//   playDispatch({ type: INIT_GAME });
+// };
 
-  // /**
-  //  * Resets all crossmarks
-  //  */
-  // const clearCrossmarks = () => {
-  //   playDispatch({ type: CLEAR_CROSSMARKS });
-  // };
+// /**
+//  * Creates a new card and stores it in state
+//  */
+// const setCard = () => {
+//   const [card, serial] = newCard(BINGO);
+//   playDispatch({ type: NEW_CARD, payload: { card: card, serial: serial } });
+// };
 
-  // TODO IS THIS NECESSARY???
-  // const updateCrossmarks = (crossmark: { [x: string]: boolean }) => {
-  //   playDispatch({ type: UPDATE_CROSSMARKS, payload: crossmark });
-  // };
+// /**
+//  * Resets all crossmarks
+//  */
+// const clearCrossmarks = () => {
+//   playDispatch({ type: CLEAR_CROSSMARKS });
+// };
 
-  // TODO IS THIS NECESSARY???
-  /**
-   * Toggle current target's crossmark visibility
-   * @param event Click event
-   */
-  // const toggleCrossmark = (event: React.MouseEvent) => {
-  //   return;
-  // let target = event.target as HTMLDivElement;
-  // let value = crossmarks[target.id];
-  // let crossmark = { [target.id]: !value };
-  // updateCrossmarks(crossmark);
-  // };
+// TODO IS THIS NECESSARY???
+// const updateCrossmarks = (crossmark: { [x: string]: boolean }) => {
+//   playDispatch({ type: UPDATE_CROSSMARKS, payload: crossmark });
+// };
 
-  // /**
-  //  * Dispatch room abandoned
-  //  */
-  // const dispatchRoomAbandoned = () => {
-  //   playDispatch({ type: PLAYER_KICKED, payload: 'abandoned' });
-  // };
+// TODO IS THIS NECESSARY???
+/**
+ * Toggle current target's crossmark visibility
+ * @param event Click event
+ */
+// const toggleCrossmark = (event: React.MouseEvent) => {
+//   return;
+// let target = event.target as HTMLDivElement;
+// let value = crossmarks[target.id];
+// let crossmark = { [target.id]: !value };
+// updateCrossmarks(crossmark);
+// };
 
-  // /**
-  //  * Dispatch player kicked
-  //  */
-  // const dispatchPlayerKicked = () => {
-  //   playDispatch({ type: PLAYER_KICKED, payload: 'banned' });
-  // };
-
-  return {
-    playState,
-    playDispatch,
-  };
-}
+//   return {
+//     playState,
+//     playDispatch,
+//   };
+// }
