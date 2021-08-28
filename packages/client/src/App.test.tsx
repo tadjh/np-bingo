fbc25e6713c697a07763e318171ed4ff4196358d
TadjhBrooks <tadjh.brooks@gmail.com>
2021-08-27 17:33:05 -0700

    Change test description

--- packages/client/src/App.test.tsx
+++ packages/client/src/App.test.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
 // TODO write test?
-test.skip('renders learn react link', () => {
+it.skip('renders home by default', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
