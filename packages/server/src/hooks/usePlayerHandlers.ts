8f4924638f317eab582e8d66e9d879f42e93c244
TadjhBrooks <tadjh.brooks@gmail.com>
2021-08-28 11:35:07 -0700

    Handle player send card

--- packages/server/src/hooks/usePlayerHandlers.ts
+++ packages/server/src/hooks/usePlayerHandlers.ts
@@ -1,40 +1,10 @@
 import { Server, Socket } from 'socket.io';
 import { Room, SocketId } from 'socket.io-adapter';
-import { Card, Player, PlayerAction } from '@np-bingo/types';
+import { Card, Player, PlayerEvent } from '@np-bingo/types';
 import { useCommonHandlers } from './useCommonHandlers';
 
 export function usePlayerHandlers(io: Server, socket: Socket) {
-  const { leaveRoom } = useCommonHandlers(io, socket);
-
-  /**
-   * Player: Action handler
-   * @param action
-   * @param room
-   * @param hostSocketId
-   * @param player
-   * @param card (optional)
-   */
-  const playerAction = (
-    action: PlayerAction,
-    room: Room,
-    hostSocketId: SocketId,
-    player: Player,
-    card?: Card
-  ) => {
-    switch (action) {
-      case 'join-room':
-        joinRoom(room, hostSocketId, player);
-        break;
-      case 'leave-room':
-        playerLeaveRoom(room, hostSocketId, player);
-        break;
-      case 'ready-up':
-        readyUp(room, hostSocketId, player);
-        break;
-      default:
-        throw new Error('Invalid player action');
-    }
-  };
+  const { emitLeaveRoom, emitRoomCheckCard } = useCommonHandlers(io, socket);
 
   /**
    * To Host: Player Join room
@@ -45,7 +15,7 @@ export function usePlayerHandlers(io: Server, socket: Socket) {
   const joinRoom = (room: Room, hostSocketId: SocketId, player: Player) => {
     socket.join(room);
     console.log(`Room ${room}: ${player.name} joined`);
-    io.to(hostSocketId).emit('host:player-action', 'join-room', player);
+    io.to(hostSocketId).emit('host:player-event', 'join-room', player);
   };
 
   /**
@@ -59,8 +29,8 @@ export function usePlayerHandlers(io: Server, socket: Socket) {
     hostSocketId: SocketId,
     player: Player
   ) => {
-    io.to(hostSocketId).emit('host:player-action', 'leave-room', player);
-    leaveRoom(room, player.name);
+    io.to(hostSocketId).emit('host:player-event', 'leave-room', player);
+    emitLeaveRoom(room, player.name);
   };
 
   /**
@@ -71,8 +41,59 @@ export function usePlayerHandlers(io: Server, socket: Socket) {
    */
   const readyUp = (room: Room, hostSocketId: SocketId, player: Player) => {
     console.log(`Room ${room}: ${player.name} is ready`);
-    io.to(hostSocketId).emit('host:player-action', 'ready-up', player);
+    io.to(hostSocketId).emit('host:player-event', 'ready-up', player);
   };
 
-  return { playerAction, playerLeaveRoom };
+  /**
+   * To Host & Room: Player sent a card
+   * @param room
+   * @param hostSocketId
+   * @param player
+   * @param card
+   */
+  const sendCard = (
+    room: Room,
+    hostSocketId: SocketId,
+    player: Player,
+    card: Card
+  ) => {
+    console.log(`Room ${room}: ${player.name} sent a card`);
+    io.to(hostSocketId).emit('host:player-event', 'send-card', player, card);
+    emitRoomCheckCard(room, player.name);
+  };
+
+  /**
+   * Player: Event listener
+   * @param event
+   * @param room
+   * @param hostSocketId
+   * @param player
+   * @param card (optional)
+   */
+  const playerEventsListener = (
+    event: PlayerEvent,
+    room: Room,
+    hostSocketId: SocketId,
+    player: Player,
+    card?: Card
+  ) => {
+    switch (event) {
+      case 'join-room':
+        joinRoom(room, hostSocketId, player);
+        break;
+      case 'leave-room':
+        playerLeaveRoom(room, hostSocketId, player);
+        break;
+      case 'ready-up':
+        readyUp(room, hostSocketId, player);
+        break;
+      case 'send-card':
+        if (!card) return;
+        sendCard(room, hostSocketId, player, card);
+      default:
+        throw new Error('Invalid player event');
+    }
+  };
+
+  return { playerEventsListener };
 }
