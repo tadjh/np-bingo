e090644e63aa3d282f4aa43761604132fbaeffce
TadjhBrooks <tadjh.brooks@gmail.com>
2021-09-01 10:45:35 -0700

    Fix centering

--- packages/client/src/components/Inputs/IconMenu/components/IconMenu/index.tsx
+++ packages/client/src/components/Inputs/IconMenu/components/IconMenu/index.tsx
@@ -26,13 +26,13 @@ export default function IconMenu({
   return (
     <div
       className={clsx(
-        'relative block w-[44px] h-[44px]',
+        'relative block w-[44px] h-[44px] flex justify-center',
         isOpen || 'overflow-hidden hover:overflow-visible'
       )}
     >
       <ul
         className={clsx(
-          'absolute flex p-1 transition-all duration-75 rounded-full border-2 gap-1',
+          'absolute flex p-1 transition-all duration-75 rounded-full border-2 gap-1 -bottom-1',
           isOpen
             ? 'z-50 bg-gray-200 dark:bg-gray-700 shadow-2xl border-gray-300 dark:border-gray-600'
             : 'border-transparent',
