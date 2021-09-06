70a3d7f41ba41f3dd32d31d5e6372ec7c6b79b62
TadjhBrooks <tadjh.brooks@gmail.com>
2021-09-06 02:03:20 -0500

    Fix PlayerName spacing

--- packages/client/src/components/Display/PlayerName/index.tsx
+++ packages/client/src/components/Display/PlayerName/index.tsx
@@ -33,36 +33,38 @@ export default function PlayerName({
   const [playerName, randomId] = name.split('#', 2);
   return (
     <div className="flex flex-row items-center gap-2 group">
-      <div className="w-4 h4">
-        {!isLoading ? '\xa0' : <Spinner className="h-4 w-4" />}
-      </div>
-      <div className="w-[86px] flex justify-end">
-        <div
-          className={clsx(
-            'flex flex-row justify-between items-center gap-1 rounded-xl bg-gray-300 dark:bg-gray-800 px-1 py-0.5 transition duration-500',
-            !status && 'opacity-50'
-          )}
-        >
-          <Alert status={playerNameAlertStyle()} />
-          <Typography className="text-black dark:text-white text-opacity-40 dark:text-opacity-40 text-xs">
-            {status ? 'Connected' : 'Offline'}
-          </Typography>
-          <div />
+      <div className="flex">
+        <div className="w-4 h4">
+          {!isLoading ? '\xa0' : <Spinner className="h-4 w-4" />}
+        </div>
+        <div className="w-[86px] flex justify-end">
+          <div
+            className={clsx(
+              'flex flex-row justify-between items-center gap-1 rounded-xl bg-gray-300 dark:bg-gray-800 px-1 py-0.5 transition duration-500',
+              !status && 'opacity-50'
+            )}
+          >
+            <Alert status={playerNameAlertStyle()} />
+            <Typography className="text-black dark:text-white text-opacity-40 dark:text-opacity-40 text-xs">
+              {status ? 'Connected' : 'Offline'}
+            </Typography>
+            <div />
+          </div>
         </div>
       </div>
-      <Typography className="flex flex-row items-center text-black dark:text-white text-opacity-90 dark:text-opacity-90 gap-x-1">
-        <div>
+      <div>
+        <Typography className="flex flex-row items-center text-black dark:text-white text-opacity-90 dark:text-opacity-90 gap-x-1">
           <span>{playerName}</span>
           <span className="text-black dark:text-white text-opacity-40 dark:text-opacity-40">
             {'#' + randomId}
           </span>
-        </div>
+        </Typography>
         {editable && (
           <IconButton className="invisible group-hover:visible">
             <PencilIcon size="x-small" />
           </IconButton>
         )}
-      </Typography>
+      </div>
     </div>
   );
 }
