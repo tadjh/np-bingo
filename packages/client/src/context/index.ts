c5eb8a212892cab9ec10b080798bf6e860075560
TadjhBrooks <tadjh.brooks@gmail.com>
2021-09-07 17:47:20 -0700

    Remove empty module

--- packages/client/src/context/index.ts
+++ packages/client/src/context/index.ts
@@ -1,7 +1,6 @@
 export * from './BallContext';
 export * from './FeaturesContext';
 export * from './GameContext';
-export * from './PlayContext';
 export * from './RoomContext';
 export * from './SoundContext';
 export * from './ThemeContext';
