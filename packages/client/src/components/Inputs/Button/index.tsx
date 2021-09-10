454d972cb4f8afaffd68587db4e6412a946d80cc
TadjhBrooks <tadjh.brooks@gmail.com>
2021-09-09 18:06:37 -0700

    Add forwardRef to Button

--- packages/client/src/components/Inputs/Button/index.tsx
+++ packages/client/src/components/Inputs/Button/index.tsx
@@ -1,4 +1,4 @@
-import React, { ButtonHTMLAttributes, useRef } from 'react';
+import { ButtonHTMLAttributes, forwardRef, useRef } from 'react';
 import clsx from 'clsx';
 import Ripple from '../../Feedback/Ripple';
 import { useButton } from './hooks';
@@ -15,32 +15,61 @@ export interface ButtonProps
   disabled?: boolean;
 }
 
-export default function Button({
-  variant = 'default',
-  component: Component,
-  className,
-  children,
-  to,
-  disabled = false,
-  ...props
-}: ButtonProps): JSX.Element {
-  const buttonRef = useRef<HTMLButtonElement | null>(null);
-  const { isRippling, coordinates, handleSetCoordinates } =
-    useRipple(buttonRef);
-  const { buttonStyle, handleMouseDown } = useButton(
-    variant,
-    disabled,
-    handleSetCoordinates
-  );
-  if (Component)
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      variant = 'default',
+      component: Component,
+      className,
+      children,
+      to,
+      disabled = false,
+      ...props
+    }: ButtonProps,
+    ref
+  ) => {
+    const childRef = useRef<HTMLSpanElement>(null);
+    const { isRippling, coordinates, handleSetCoordinates } = useRipple(
+      childRef.current?.parentElement || null
+    );
+    const { buttonStyle, handleMouseDown } = useButton(
+      variant,
+      handleSetCoordinates
+    );
+
+    if (Component)
+      return (
+        <Component
+          {...props}
+          ref={ref}
+          to={to}
+          className={clsx(
+            'relative px-6 py-2 rounded-full transition focus:outline-none overflow-hidden text-center',
+            'disabled:opacity-50 disabled:text-gray-800 dark:disabled:text-gray-400 disabled:shadow-none disabled:cursor-default disabled:pointer-events-none',
+            buttonStyle(),
+            className
+          )}
+          onMouseDown={handleMouseDown}
+          disabled={disabled}
+        >
+          {isRippling && (
+            <Ripple
+              style={{ top: `${coordinates.y}px`, left: `${coordinates.x}px` }}
+              disabled={disabled}
+            />
+          )}
+          <span ref={childRef} className="relative z-10">
+            {children}
+          </span>
+        </Component>
+      );
     return (
-      <Component
+      <button
         {...props}
-        ref={buttonRef}
-        to={to}
+        ref={ref}
         className={clsx(
           'relative px-6 py-2 rounded-full transition focus:outline-none overflow-hidden text-center',
-            'disabled:opacity-50 disabled:text-gray-800 dark:disabled:text-gray-400 disabled:shadow-none disabled:cursor-default disabled:pointer-events-none',
+          'disabled:opacity-50 disabled:text-gray-800 dark:disabled:text-gray-400 disabled:shadow-none disabled:cursor-default disabled:pointer-events-none',
           buttonStyle(),
           className
         )}
@@ -53,29 +82,12 @@ export default function Button({
             disabled={disabled}
           />
         )}
-        <span className="relative z-10">{children}</span>
-      </Component>
+        <span ref={childRef} className="relative z-10">
+          {children}
+        </span>
+      </button>
     );
-  return (
-    <button
-      {...props}
-      ref={buttonRef}
-      className={clsx(
-        'relative px-6 py-2 rounded-full transition focus:outline-none overflow-hidden text-center',
-          'disabled:opacity-50 disabled:text-gray-800 dark:disabled:text-gray-400 disabled:shadow-none disabled:cursor-default disabled:pointer-events-none',
-        buttonStyle(),
-        className
-      )}
-      onMouseDown={handleMouseDown}
-      disabled={disabled}
-    >
-      {isRippling && (
-        <Ripple
-          style={{ top: `${coordinates.y}px`, left: `${coordinates.x}px` }}
-          disabled={disabled}
-        />
-      )}
-      <span className="relative z-10">{children}</span>
-    </button>
-  );
-}
+  }
+);
+
+export default Button;
