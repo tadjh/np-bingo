f2df6e01e183acb26eab146a81d0fac47253381b
TadjhBrooks <tadjh.brooks@gmail.com>
2021-09-11 14:17:25 -0700

    Use clsx

--- packages/client/src/components/Feedback/Modal/components/ModalBase/index.tsx
+++ packages/client/src/components/Feedback/Modal/components/ModalBase/index.tsx
@@ -1,10 +1,11 @@
-import { Fragment } from 'react';
-export interface ModalBaseProps extends React.HTMLAttributes<HTMLDivElement> {
+import { Fragment, HTMLAttributes } from 'react';
+import clsx from 'clsx';
+export interface ModalBaseProps extends HTMLAttributes<HTMLDivElement> {
   onClose?: () => void;
 }
 export default function ModalBase({
   onClose,
-  className = '',
+  className,
   children,
 }: ModalBaseProps): JSX.Element {
   return (
@@ -14,7 +15,11 @@ export default function ModalBase({
         onClick={onClose}
       ></div>
       <div
-        className={`absolute z-50 w-11/12 rounded-md flex flex-col gap-5 bg-gray-100 dark:bg-gray-800 border-gray-300 dark:border-gray-600 text-black dark:text-white text-opacity-90 dark:text-opacity-90 overflow-hidden shadow-md ${className}`}
+        className={clsx(
+          'absolute z-50 w-11/12 rounded-md flex flex-col gap-5 overflow-hidden shadow-md',
+          'bg-gray-100 dark:bg-gray-800 border-gray-300 dark:border-gray-600 text-black dark:text-white text-opacity-90 dark:text-opacity-90',
+          className
+        )}
       >
         {children}
       </div>
