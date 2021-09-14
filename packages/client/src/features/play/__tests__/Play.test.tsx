dc620c2b1484721790d39e9fe4df2babb3e116ec
TadjhBrooks <tadjh.brooks@gmail.com>
2021-09-13 21:05:55 -0700

    Clean up

--- packages/client/src/features/play/__tests__/Play.test.tsx
+++ packages/client/src/features/play/__tests__/Play.test.tsx
@@ -14,13 +14,8 @@ import {
   UserContext,
 } from '../../../context';
 import { READY_CHECK, START } from '../../../config/constants';
-import { useAppState, useApp } from '../../../hooks';
-import { act } from 'react-dom/test-utils';
+import { useAppState } from '../../../hooks';
 import userEvent from '@testing-library/user-event';
-// import { createServer } from 'http';
-// import { Server, Socket as ServerSocket } from 'socket.io';
-// import Client, { Socket as ClientSocket } from 'socket.io-client';
-// import { AddressInfo } from 'net';
 import { Gamestate, Room } from '@np-bingo/types';
 import socket from '../../../lib/socket.io';
 
@@ -73,32 +68,9 @@ function PlayWithContext({
 }
 
 describe('Play unit tests', () => {
-  // let io: Server, serverSocket: ServerSocket, clientSocket: ClientSocket;
-
-  // beforeAll((done) => {
-  //   const httpServer = createServer();
-  //   io = new Server(httpServer);
-  //   httpServer.listen(() => {
-  //     const address = httpServer.address() as AddressInfo;
-  //     clientSocket = Client(`http://localhost:${address.port}`);
-  //     io.on('connection', (socket) => {
-  //       socket.join(room);
-  //       serverSocket = socket;
-  //     });
-  //     clientSocket.on('connect', done);
-  //   });
-  // });
-
-  // afterAll(() => {
-  //   io.close();
-  //   clientSocket.close();
-  // });
-
   it('handles player ready', () => {
-    act(() => {
-      render(<PlayWithContext gamestateOverride="ready" />, {
-        wrapper: MemoryRouter,
-      });
+    render(<PlayWithContext gamestateOverride="ready" />, {
+      wrapper: MemoryRouter,
     });
 
     const bingoButton = screen.getByRole('button', { name: /bingo/i });
@@ -109,23 +81,17 @@ describe('Play unit tests', () => {
     ).toBeInTheDocument();
     expect(bingoButton).toBeDisabled();
 
-    act(() => {
-      userEvent.click(readyButton);
-    });
+    userEvent.click(readyButton);
 
     expect(readyButton).toBeDisabled();
     expect(
       screen.getByText(/waiting for host to dispense a ball\.\.\./i)
     ).toBeInTheDocument();
-
-    // done();
   });
 
   it('handles send card', () => {
-    act(() => {
-      render(<PlayWithContext gamestateOverride="start" />, {
-        wrapper: MemoryRouter,
-      });
+    render(<PlayWithContext gamestateOverride="start" />, {
+      wrapper: MemoryRouter,
     });
 
     const readyButton = screen.getByRole('button', { name: /ready/i });
@@ -137,12 +103,9 @@ describe('Play unit tests', () => {
     ).toBeInTheDocument();
     expect(bingoButton).toBeEnabled();
 
-    act(() => {
-      userEvent.click(bingoButton);
-    });
+    userEvent.click(bingoButton);
 
     expect(screen.getByText(/sending card to host\.\.\./i)).toBeInTheDocument();
     expect(bingoButton).toBeDisabled();
-    // done();
   });
 });
