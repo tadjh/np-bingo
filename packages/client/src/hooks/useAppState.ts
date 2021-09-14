11b8e8ba1d69f12cf78c550b401a84dde7c9552f
TadjhBrooks <tadjh.brooks@gmail.com>
2021-09-13 18:26:12 -0700

    Clean up

--- packages/client/src/hooks/useAppState.ts
+++ packages/client/src/hooks/useAppState.ts
@@ -16,67 +16,6 @@ export function useAppState() {
     initialAppState
   );
 
-  // /**
-  //  * Manage game state
-  //  * @param gamestate
-  //  */
-  // const play = useCallback((gamestate: Gamestate) => {
-  //   switch (gamestate) {
-  //     case 'init':
-  //       dispatch({ type: INIT_GAME });
-  //       break;
-  //     case 'ready':
-  //       dispatch({ type: READY_CHECK });
-  //       break;
-  //     case 'standby':
-  //       dispatch({ type: STANDBY });
-  //       break;
-  //     case 'start':
-  //       dispatch({ type: START_GAME });
-  //       break;
-  //     case 'validate':
-  //       dispatch({ type: VALIDATE });
-  //       break;
-  //     case 'pause':
-  //       dispatch({ type: PAUSE });
-  //       break;
-  //     case 'failure':
-  //       dispatch({ type: FAILURE });
-  //       break;
-  //     case 'win':
-  //       dispatch({ type: WIN_GAME });
-  //       break;
-  //     case 'end':
-  //       dispatch({ type: END_GAME });
-  //       break;
-  //     default:
-  //       throw new Error('Invalid game state.');
-  //   }
-  // }, []);
-
-  // /**
-  //  * Update game mode
-  //  * @param gamemode
-  //  */
-  // const mode = useCallback((gamemode: Gamemode) => {
-  //   switch (gamemode) {
-  //     case 'default':
-  //       dispatch({ type: UPDATE_GAMEMODE, payload: 'default' });
-  //       break;
-  //     case 'solo':
-  //       dispatch({ type: UPDATE_GAMEMODE, payload: 'solo' });
-  //       break;
-  //     case 'death':
-  //       dispatch({ type: UPDATE_GAMEMODE, payload: 'death' });
-  //       break;
-  //     case 'blackout':
-  //       dispatch({ type: UPDATE_GAMEMODE, payload: 'blackout' });
-  //       break;
-  //     default:
-  //       throw new Error('Invalid game mode.');
-  //   }
-  // }, []);
-
   return {
     state,
     dispatch,
